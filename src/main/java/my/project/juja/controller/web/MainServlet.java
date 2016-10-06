fc0d6281a9b1648bfda59e11d3c1ca1c2d18c3e2
Nikolay <Horushko.Nikolay@gmail.com>
2016-10-06 20:47:22 +0300

    create MainServlet

---
+++ src/main/java/my/project/juja/controller/web/MainServlet.java
@@ -0,0 +1,16 @@
+package my.project.juja.controller.web;
+
+import java.io.IOException;
+
+/**
+ * Created by Nikol on 10/6/2016.
+ */
+public class MainServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}

ff1f0ea32c6ec2130c763d7cb35f6a395f9dfe60
Nikolay <Horushko.Nikolay@gmail.com>
2017-01-01 19:44:09 +0300

    connect spring

--- src/main/java/my/project/juja/service/DatabaseManagerFactoryImpl.java
+++ src/main/java/my/project/juja/service/DatabaseManagerFactoryImpl.java
@@ -1,20 +1,20 @@
 package my.project.juja.service;
 
 import my.project.juja.model.DatabaseManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Created by Nikol on 11/23/2016.
  */
 public class DatabaseManagerFactoryImpl implements DatabaseManagerFactory {
     private String className;
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Override
     public DatabaseManager createDatabaseManger(){
-        try {
-            return (DatabaseManager) Class.forName(className).newInstance();
-        } catch (Exception e) {
-            throw  new RuntimeException(e);
-        }
+        return (DatabaseManager) applicationContext.getBean(className);
     }
 
     public void setClassName(String className) {
